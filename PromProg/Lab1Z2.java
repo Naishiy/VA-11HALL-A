cc6811eca3390b6af38186f3086a1b50afd3c499
Naishiy <159252243+Naishiy@users.noreply.github.com>
2024-02-12 00:49:10 +0300

    1 и 2 задание Промышленное программирование

---
+++ PromProg/Lab1Z2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Lab1Z2 {
+
+    public static void main(String[] args) {
+        /*Задание 2.6*/
+        Scanner in = new Scanner(System.in);
+        System.out.print("Сколько чисел вы хотите ввести: ");
+        int n = in.nextInt();
+
+        int[] array = new int [n];
+        for(int i = 0; i < n; i++)
+        {
+            i++;
+            System.out.print("Число " + i + " = ");
+            i--;
+            array[i] = in.nextInt();
+        }
+        System.out.println("Простыми являются:");
+
+        for(int i = 0; i < n; i++){
+            boolean f = true;
+
+            for (int a = 2; a < array[i]; a++){
+
+                if(array[i] % a == 0){
+                    f = false;
+                    break;
+                }
+            }
+
+            if(f){
+                System.out.print(array[i] + " ");
+            }
+        }
+        in.close();
+    }
+}
