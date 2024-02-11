917c5276996ff8962d0dcff8fc082e952ef2a120
Naishiy <159252243+Naishiy@users.noreply.github.com>
2024-02-12 01:31:45 +0300

    1 Лаба Промышленное программирование

---
+++ PromProg/Lab1/Lab1Z1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Lab1Z1 {
+    public static void main() {
+        /*Задание 1.6*/
+        Scanner in = new Scanner(System.in);
+        System.out.print("Текущая дата и время: ");
+        String date1 = in.nextLine();
+        String name = "Аверин, Маликова, Сафонова";
+        String date2 = "05.02.2024, 14:58";
+        System.out.println("\nРазработчики: " + name + "\nДата и время получения задания: " + date2 + "\nДата и время сдачи задания: " + date1 + "\n");
+    }
+}
---
+++ PromProg/Lab1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Lab1Z1 class1 = new Lab1Z1();
+        Lab1Z2 class2 = new Lab1Z2();
+
+        class1.main();
+        class2.main();
+    }
+}
