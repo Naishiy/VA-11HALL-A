da704b5e813c51936b8756c17b17717104997383
Naishiy <159252243+Naishiy@users.noreply.github.com>
2024-02-12 00:56:43 +0300

    Update Lab1Z1.java

--- PromProg/Lab1Z1.java
+++ PromProg/Lab1Z1.java
@@ -11,32 +11,6 @@ public class Lab1Z1 {
         String date2 = "05.02.2024, 14:58";
         System.out.println("\nРазработчики: " + name + "\nДата и время получения задания: " + date2 + "\nДата и время сдачи задания: " + date1 + "\n");
 
-        /*Задание 2.6*/
-        System.out.print("Сколько чисел вы хотите ввести: ");
-        int n = in.nextInt();
-
-        int[] array = new int [n];
-        for(int i = 0; i < n; i++)
-        {
-            array[i] = in.nextInt();
-        }
-        System.out.println("Простыми являются:");
-
-        for(int i = 0; i < n; i++){
-            boolean f = true;
-
-            for (int a = 2; a < array[i]; a++){
-
-                if(array[i] % a == 0){
-                    f = false;
-                    break;
-                }
-            }
-
-            if(f){
-                System.out.print(array[i] + " ");
-            }
-        }
         in.close();
     }
 }
