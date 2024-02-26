600a2f6d163fc8bdc37386c35f0b94456a697f14
DaryaSafonova <159729250+DaryaSafonova@users.noreply.github.com>
2024-02-26 03:13:30 +0300

    Update Lab2Z1.java
    1-е задание с учетом замечаний

--- PromProg/Lab2/Lab2Z1.java
+++ PromProg/Lab2/Lab2Z1.java
@@ -8,15 +8,39 @@ public class Lab2Z1 {
         System.out.print("Введите несколько чисел: ");
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
+        String[] numbers = line.split(" ");
 
         /*Считывание отдельных чисел*/
-        for (String s : line.split(" ")) {
+        for (String s: numbers) {
 
+            boolean isNumber = s.matches("\\d+");
             /*Проверка состоит ли следующий после пробела набор символов из цифр*/
-            if (s.matches("\\d+")) {
+            if (isNumber) {
 
-                System.out.print("Первым числом, состоящим только из чисел является: " + s + "\n");
-                break;
+                /*Строка для проверки на повторяющиеся цифры*/
+                String duplicanty = new String();
+
+                /*Счетчик повторений цифр в числе*/
+                boolean flag = true;
+
+                /*Проверка состоит ли следующий после пробела набор символов из разных цифр*/
+                for (String ch : s.split("")) {
+
+                    /*Проверка на повторное содержание символа в числе*/
+                    if (duplicanty.contains(ch)) {
+                        flag = false;
+                        break;
+                    }
+
+                    duplicanty += ch;
+                }
+
+                /*Вывод первого числа, состоящего только из различных цифр*/
+                if (flag) {
+
+                    System.out.print("Первым числом, состоящим только из различных цифр является: " + s + "\n");
+                    break;
+                }
             }
         }
     }
