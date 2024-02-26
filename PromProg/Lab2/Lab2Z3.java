7b3bbcecbea29263f2b93b51a94e2d9112f5fe44
DaryaSafonova <159729250+DaryaSafonova@users.noreply.github.com>
2024-02-26 03:04:11 +0300

    Update Lab2Z3.java
    Исправленный код 3-го задания

--- PromProg/Lab2/Lab2Z3.java
+++ PromProg/Lab2/Lab2Z3.java
@@ -9,35 +9,46 @@ public class Lab2Z3 {
         System.out.print("\nВведите размерность матрицы: ");
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        float[][] A = new float[n][n];
+        if (n <= 0) {
+            System.out.println("\nВведена неккоректная размерность");
+        }
 
-        /*Заполнение матрицы*/
+        else {
+            float[][] A = new float[n][n];
+
+            /*Заполнение матрицы*/
+            fill(n, A);
+
+            // Выводим матрицу на консоль
+            System.out.println("\nМатрица A до округления:");
+            output(n, A);
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    A[i][j] = Math.round(A[i][j]);
+                }
+            }
+            System.out.println("\nМатрица A после округления:");
+            output(n, A);
+        }
+
+    }
+
+    private static void output(int n, float[][] A) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fill(int n, float[][] A) {
         Random random = new Random();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 A[i][j] = random.nextFloat() * (2 * n + 1) - n;
             }
-        }
-// Выводим матрицу на консоль
-        System.out.println("\nМатрица A до округления:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(A[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                A[i][j] = Math.round(A[i][j]);
-            }
-        }
-        System.out.println("\nМатрица A после округления:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(A[i][j] + " ");
-            }
-            System.out.println();
         }
     }
 }
