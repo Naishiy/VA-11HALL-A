e197b92c4343cb4ada33715162866da567204ce6
tsukiyoomi <159731606+tsukiyoomi@users.noreply.github.com>
2024-03-06 23:40:55 +0300

    Update Лаб1.py
    коммы добавила

--- DiscMath/Лаб1.py
+++ DiscMath/Лаб1.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-n = 7
-m = 10
+n = 7 #Количество строк и столбцов
+m = 10 #Количество столбцов в инцидентности
 
+#Исходная матрица смежности:
 A = [[0, 1, 1, 0, 0, 1, 1],  # x1
      [1, 0, 1, 0, 0, 0, 0],  # x2
      [1, 1, 0, 1, 0, 0, 0],  # x3
@@ -11,42 +12,42 @@ A = [[0, 1, 1, 0, 0, 1, 1],  # x1
      [1, 0, 0, 1, 1, 0, 0],  # x6
      [1, 0, 0, 0, 1, 0, 0]]  # x7
 
-def adj_to_inc(adj_matrix):
+def adj_to_inc(adj_matrix): #Смежность в инцидентность
 
-    n = len(adj_matrix)
-    m = n * (n - 1) // 2
+    n = len(adj_matrix) #Размер матрицы
+    m = 10 #Количество столбцов в матрице инцидентности
 
-    inc_matrix = [[0] * m for _ in range(n)]
-    edge_index = 0
+    inc_matrix = [[0] * m for _ in range(n)] #Создание пустой матрицы инцидентности
+    edge_index = 0 #Счетчик ребер
 
-    for i in range(n):
-        for j in range(i + 1, n):
-            if adj_matrix[i][j] != 0:
-                inc_matrix[i][edge_index] = 1
+    for i in range(n): #Перебор строк
+        for j in range(i + 1, n): #Перебор столбцов
+            if adj_matrix[i][j] != 0: #Если элемент ij матрицы смежности не равен нулю:
+                inc_matrix[i][edge_index] = 1 #Присваивание значения элементам матрицы инцидентности
                 inc_matrix[j][edge_index] = 1
-                edge_index += 1
+                edge_index += 1 #Счетчик ребер +1
 
     return inc_matrix
 
 
-def inc_to_adj(inc_matrix):
-    n = len(inc_matrix)
-    m = len(inc_matrix[0])
+def inc_to_adj(inc_matrix): #Инцидентность в смежность
+    n = len(inc_matrix)    #Колво строк матрицы инц
+    m = len(inc_matrix[0]) #Колво столбцов матрицы инц
 
-    adj_matrix = [[0] * n for _ in range(n)]
+    adj_matrix = [[0] * n for _ in range(n)] #Создание пустой матрицы смежности
 
-    for edge in range(m):
-        for vertex in range(n):
-            if inc_matrix[vertex][edge] == 1:
-                for vertex2 in range(n):
-                    if inc_matrix[vertex2][edge] == 1 and vertex != vertex2:
-                        adj_matrix[vertex][vertex2] = 1
+    for edge in range(m): #Прохождение по столбцам
+        for vertex in range(n): #Прохождение по строкам
+            if inc_matrix[vertex][edge] == 1: #Если элемент матрицы инцидентности = 1
+                for vertex2 in range(n): #Прохождение по строкам снова
+                    if inc_matrix[vertex2][edge] == 1 and vertex != vertex2: #Если элемент матрицы инцидентности = 1 и мы не на главной диагонали:
+                        adj_matrix[vertex][vertex2] = 1 #Присваиваем значения элементам матрицы смежности
                         adj_matrix[vertex2][vertex] = 1
                         break
 
     return adj_matrix
 
-
+############################################################
 print('Начальная Матрица Смежности:')
 print(f'    x1  x2  x3  x4  x5  x6  x7')
 
